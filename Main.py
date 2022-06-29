5ab90f71328e25acf8a5a5d795a5ec26650e7ee1
jmonsalvo <jmonsa13@gmail.com>
2022-06-29 18:56:22 -0500

    Capture of all the quality general data and their labels using tidy data format

--- Main.py
+++ Main.py
@@ -7,10 +7,9 @@
 # ----------------------------------------------------------------------------------------------------------------------
 import os
 import datetime
-from functions.select_files import *
-
 import pandas as pd
-import numpy as np
+
+from functions.select_files import convert_date_format, find_type_files, find_day_files, find_range_day_files
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Functions
@@ -28,7 +27,7 @@ format_path = os.path.join(source_dir, 'Artik blanco')
 type_file = 'PartialStat'
 
 # Range of data
-info_date = 'day'  # 'range'
+info_date = 'range_test'  # 'range'
 if info_date == 'day':
     day_filter_ini = '14_05_2022'
 elif info_date == 'range':
@@ -37,6 +36,8 @@ elif info_date == 'range':
 elif info_date == 'range_test':
     day_filter_ini = '14_05_2022'
     day_filter_fin = '15_05_2022'
+
+general_column = ['Fecha', 'Tono', 'Calidad', 'Valor_und']
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Main Code
@@ -50,9 +51,68 @@ if info_date == 'day':
 elif info_date == 'range_test':
     files_filter = find_range_day_files(type_files, day_filter_ini, day_filter_fin)
 
+# TODO: Sort the files by date using their name information and create a df with 3 columns (filename, date(datetime),
+#  info_group)
 
-for file in files_filter:
-    print(file)
+# Empty list
+general_data = []
+
+# Row assign
+row_data = 20
+
+for filename in files_filter:
+    # Flag to avoid capture general data
+    flag_datos_gen = True
+
+    # Filename path
+    filename_path = os.path.join(format_path, filename)
+
+    # Reading the lines of the files
+    with open(filename_path, 'r', encoding="utf-16") as f:
+        lines = f.readlines()
+
+    # Capturing the date (last row)
+    date_str = lines[-1].strip('SYSTEM').strip()
+    fecha = datetime.datetime.strptime(date_str, '%d/%m/%Y, %H:%M:%S')
+
+    # Capturing the quality
+    for i, line in enumerate(lines[10:]):
+        # Checking for empty line
+        if line != "\n":
+            # Checking for quality data
+            if line.split()[0] == 'Primera':
+                if flag_datos_gen is False:
+                    # Tone color
+                    tone = lines[i+10-2].split(' ')[0].strip()
+                    # General Quality
+                    for j in range(i+10, i+10+5, 1):
+                        # capture value
+                        calidad_str = lines[j].split()[0]
+                        calidad_und = int(lines[j].split()[-1])
+
+                        general_data.append([fecha, tone, calidad_str, calidad_und])
+                elif flag_datos_gen is True:
+                    flag_datos_gen = False
+
+        # Capturing the 3 last format
+        elif line == "\n":
+            # Tone color
+            tone = lines[i + 13].split()[0]
+            # General Quality
+            for j in range(i + 14, i + 14 + 2, 1):
+                # capture value
+                calidad_str = " ".join(lines[j].split()[0:2])
+                calidad_und = int(lines[j].split()[-1])
+
+                general_data.append([fecha, tone, calidad_str, calidad_und])
+            break  # Exit the for loop
+
+# Creation of the Dataframe with general quality data
+df = pd.DataFrame(general_data, columns=general_column)
+
+# Export the General Quality
+df.to_excel('General_quality.xlsx', index=False)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Executing CODE
@@ -60,11 +120,7 @@ for file in files_filter:
 def qualitron_main():
     print('Hola')
 
-
-
-
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     print('Running the Qualitron data mining process')
     qualitron_main()
-
